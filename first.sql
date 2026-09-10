2f4e6dc0a42b6d4d3b0355e83aac4cf937582625
SHUBHXSHARMA <sharmashubh0405@gmail.com>
2026-09-10 19:20:46 +0530

    SQL

--- first.sql
+++ first.sql
@@ -4,7 +4,9 @@ USE instagram ;
 CREATE TABLE user (
       id INT ,
       name VARCHAR(30) NOT NULL,
-      email VARCHAR(50) ,
-      followers INT ,
-      following INT 
+      email VARCHAR(50) UNIQUE  ,
+      followers INT DEFAULT 0  ,
+      following INT ,
+      age INT ,
+      CONSTRAINT age_check CHECK (age >= 13  )
     );
